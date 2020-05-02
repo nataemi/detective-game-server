0835c5e53bccc642d5b4e933c2bf3547813733da
nataemi <38769153+nataemi@users.noreply.github.com>
2020-05-02 17:06:40 +0200

    Update drop_tables.sql

--- database/drop_tables.sql
+++ database/drop_tables.sql
@@ -1,3 +1,6 @@
+SET FOREIGN_KEY_CHECKS = 0;
+-- ustaw na 1 jak juz dropniesz
+
 DROP TABLE IF EXISTS answer;
 DROP TABLE IF EXISTS question;
 DROP TABLE IF EXISTS action_location;
@@ -9,6 +12,8 @@ DROP TABLE IF EXISTS location;
 DROP TABLE IF EXISTS save;
 DROP TABLE IF EXISTS item;
 DROP TABLE IF EXISTS detective_case;
+
+-- tego raczej nie potrzebujesz usuwac
 DROP TABLE IF EXISTS user_roles;
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS roles;
