7702843c36371faa731554e17f5d30aae7b681b2
Natalia G <natalia.galda@coreservices.pl>
2020-05-04 17:49:52 +0200

    locationConnection rename

--- detectivegame/src/main/java/pl/detectivegame/model/DAO/LocationConnectionIdentity.java
+++ detectivegame/src/main/java/pl/detectivegame/model/DAO/LocationConnectionIdentity.java
@@ -1,5 +1,6 @@
 package pl.detectivegame.model.DAO;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import javax.persistence.Column;
@@ -11,9 +12,11 @@ import java.io.Serializable;
 public class LocationConnectionIdentity implements Serializable {
 
     @Column(name = "from_id")
+    @JsonProperty("lcoation1")
     private long fromId;
 
     @Column(name = "to_id")
+    @JsonProperty("location2")
     private long toId;
 
 
