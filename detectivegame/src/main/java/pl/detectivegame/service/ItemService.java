c91c602990c1d60719788d4b40c332aae541382e
Natalia G <natalia.galda@coreservices.pl>
2020-05-13 23:33:20 +0200

    item revealed true

--- detectivegame/src/main/java/pl/detectivegame/service/ItemService.java
+++ detectivegame/src/main/java/pl/detectivegame/service/ItemService.java
@@ -2,23 +2,11 @@ package pl.detectivegame.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import pl.detectivegame.model.DAO.Answer;
-import pl.detectivegame.model.DAO.Question;
-import pl.detectivegame.model.DAO.QuestionWithoutAnswers;
+import pl.detectivegame.model.DAO.*;
 import pl.detectivegame.model.Item;
 import pl.detectivegame.payload.creation.ItemPayload;
-import pl.detectivegame.payload.creation.QuestionPayload;
-import pl.detectivegame.repository.AnswerRepository;
-import pl.detectivegame.repository.ItemRepository;
-import pl.detectivegame.repository.QuestionRepository;
-import pl.detectivegame.repository.QuestionWithoutAnswersRepository;
+import pl.detectivegame.repository.*;
 import pl.detectivegame.util.mapper.ItemMapper;
-import pl.detectivegame.util.mapper.QuestionMapper;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Service
 public class ItemService {
@@ -26,10 +14,17 @@ public class ItemService {
     @Autowired
     ItemRepository itemRepository;
 
+    @Autowired
+    DetectiveCaseService detectiveCaseService;
+
+    @Autowired
+    ActionItemRepository actionItemRepository;
+
     public ItemPayload saveItem(ItemPayload itemPayload) {
         Item item = itemPayload.getItem();
         pl.detectivegame.model.DAO.Item itemDAO = ItemMapper.mapToItem(item);
         itemDAO = itemRepository.save(itemDAO);
+        modifyActionItem(itemPayload,itemDAO);
         itemPayload.setItem(ItemMapper.mapToItem(itemDAO));
         return itemPayload;
     }
@@ -37,6 +32,46 @@ public class ItemService {
     public void deleteItem(ItemPayload itemPayload) {
         Item item = itemPayload.getItem();
         pl.detectivegame.model.DAO.Item itemDAO = ItemMapper.mapToItem(item);
+        deleteActionItem(itemDAO);
         itemRepository.delete(itemDAO);
+    }
+
+    public void addActionItemIfIsRevealed(ItemPayload itemPayload, pl.detectivegame.model.DAO.Item itemDAO) {
+        if(itemPayload.getItem().isRevealed()){
+            long frstActionId = detectiveCaseService.getFrstActionId(itemDAO.getCaseId());
+            ActionItem actionItem = ActionItem.builder()
+                    .actionItemIdentity(
+                            ActionItemIdentity.builder()
+                                    .actionId(frstActionId)
+                                    .itemId(itemDAO.getItemId())
+                                    .build()
+                    )
+                    .build();
+            actionItemRepository.save(actionItem);
+        }
+    }
+
+    public void modifyActionItem(ItemPayload itemPayload, pl.detectivegame.model.DAO.Item itemDAO) {
+        addActionItemIfIsRevealed(itemPayload, itemDAO);
+        deleteActionItemIfIsNotRevealed(itemPayload,itemDAO);
+    }
+
+    private void deleteActionItemIfIsNotRevealed(ItemPayload itemPayload, pl.detectivegame.model.DAO.Item itemDAO) {
+        if (!itemPayload.getItem().isRevealed()) {
+            deleteActionItem(itemDAO);
+        }
+    }
+
+    private void deleteActionItem(pl.detectivegame.model.DAO.Item itemDAO) {
+        long frstActionId = detectiveCaseService.getFrstActionId(itemDAO.getCaseId());
+        ActionItem actionItem = ActionItem.builder()
+                .actionItemIdentity(
+                        ActionItemIdentity.builder()
+                                .actionId(frstActionId)
+                                .itemId(itemDAO.getItemId())
+                                .build()
+                )
+                .build();
+        actionItemRepository.delete(actionItem);
     }
 }
